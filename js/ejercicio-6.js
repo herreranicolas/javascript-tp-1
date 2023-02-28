7ccabc1fe2ba0f751e7d34be214dcdcd90a43108
Nicolas Herrera <herreranicoo17@gmail.com>
2023-02-28 19:21:25 -0300

    Agregue el archivo ejercicio-6.js y resolví lo solicitado en el trabajo práctico

---
+++ js/ejercicio-6.js
@@ -0,0 +1,8 @@
+let primerNumero = parseInt(prompt("Ingrese un número"));
+let segundoNumero = parseInt(prompt("Ingrese otro número"));
+
+if (primerNumero > segundoNumero) {
+  document.write(`El número ${primerNumero} es el mayor de los números ingresados.`);
+} else {
+  document.write(`El número ${segundoNumero} es el mayor de los números ingresados.`);
+}
