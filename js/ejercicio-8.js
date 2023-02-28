11b8b74bc60605941aee86e5d893d6c4cfbb119d
Nicolas Herrera <herreranicoo17@gmail.com>
2023-02-28 19:37:12 -0300

    Agregue el archivo ejercicio-8.js y resolví lo solicitado en el trabajo práctico

---
+++ js/ejercicio-8.js
@@ -0,0 +1,6 @@
+let numero = parseInt(prompt("Ingrese un número"));
+if (numero % 2 === 0) {
+  document.write("El número ingresado es divisible por 2.");
+} else {
+  document.write("El número ingresado no es divisible por 2.");
+}
